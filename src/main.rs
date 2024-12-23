72e138943b77a157012a926aa8c83d8b2a313f4a
iglak <iglak.me@gmail.com>
2024-12-23 18:16:26 +0100

    confirm imports from external crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rawr::{FieldDef, Schema, SchemaDef, StructDef};
+use schemas::module::ImportedStruct;
+
+#[allow(unused)]
+#[derive(Debug, Clone)]
+struct MainStruct {
+    a: i32,
+    b: ImportedStruct,
+}
+
+impl Schema for MainStruct {
+    fn schema() -> SchemaDef {
+        SchemaDef::Struct(StructDef {
+            name: "MainStruct",
+            module_path: ::core::module_path!(),
+            fields: vec![
+                FieldDef {
+                    name: "a",
+                    schema: <i32 as Schema>::schema,
+                },
+                FieldDef {
+                    name: "b",
+                    schema: <ImportedStruct as Schema>::schema,
+                },
+            ],
+        })
+    }
+}
+
+const _: () = {
+    #[linkme::distributed_slice(rawr::SCHEMA_REGISTRY)]
+    static __: fn() -> SchemaDef = <MainStruct as Schema>::schema;
+};
+
 fn main() {
     // We need to invoke linker on the schemas crate so that the static variables
     // are actually "initialized" and added to the binary. Otherwise, the registry
