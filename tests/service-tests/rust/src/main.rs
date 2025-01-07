991f44a623d237f2d3ea71c307f3fd36247c39c4
iglak <iglak.me@gmail.com>
2025-01-07 17:47:43 +0100

    specify as a test

--- tests/service-tests/rust/src/main.rs
+++ tests/service-tests/rust/src/main.rs
@@ -1,12 +1,16 @@
 use futures::stream::{self, StreamExt};
 use schemas::service::{TestClient, TestServer, TestService};
+use tokio::time::{self, Duration};
 
 #[derive(Clone)]
 struct ServiceImpl {}
 
 impl TestService for ServiceImpl {
     async fn say_hello(&self, arg: String) -> String {
+        // To see the test fail, uncomment this line.
+        // std::thread::sleep(std::time::Duration::from_secs(1));
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
         format!("Hello, {}!", arg)
     }
 }
@@ -29,7 +33,11 @@ async fn main() {
         let response = client.say_hello(format!("World {}", i + 1)).await;
         println!("{}: {}", i + 1, response);
     };
-    stream::iter(0..10)
-        .for_each_concurrent(None, make_request)
-        .await;
+    let make_requests = stream::iter(0..10).for_each_concurrent(None, make_request);
+
+    // For posterity: https://www.youtube.com/watch?v=ms8zKpS_dZE
+    // ASSERT: Requests should be processed concurrently (take ~1s).
+    if let Err(_) = time::timeout(Duration::from_secs(2), make_requests).await {
+        panic!("for_each_concurrent took more than 2 seconds");
+    }
 }
